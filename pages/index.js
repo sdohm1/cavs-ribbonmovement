6856df690b874a587794bd72f3c9879f96ffc833
sdohm1 <stevie.dohm@gmail.com>
2025-05-13 17:24:11 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
 
 export default function RibbonBoard() {
   const [imageURL, setImageURL] = useState(null);
